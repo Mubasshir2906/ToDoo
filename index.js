5efdc1dba01761ecc2b8a1505950c547c898f66b
Mubasshir2906 <40007050+Mubasshir2906@users.noreply.github.com>
2022-03-20 21:20:14 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const menuTitle = document.querySelector("#menu")
+const menuBar = document.querySelector("#menu-bar")
+menu.addEventListener("click",()=>{
+    menuBar.classList.toggle("show");
+    console.log("Hello")
+})
